e709a52cddc65538942611a09018c5eedd9bec34
Don <fattdon@amcorner.de>
2015-11-09 16:56:07 +0100

    Nach Folge 3

--- CasualRacer/Pages/GamePage.xaml.cs
+++ CasualRacer/Pages/GamePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CasualRacer.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,6 +21,8 @@ namespace CasualRacer.Pages
     /// </summary>
     public partial class GamePage : Page
     {
+        
+
         public GamePage()
         {
             InitializeComponent();
